c84d1f3a5b073a534a4edb65b4b331e833211637
rozz3ff <serega.play2017@yandex.ru>
2024-11-20 09:35:57 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblio_book
+{
+    // Класс представляет книгу в библиотеке
+    public class Book
+    {
+        // Уникальный идентификатор книги
+        public string Id { get; set; }
+
+        // Название книги
+        public string Title { get; set; }
+
+        // Автор книги
+        public string Author { get; set; }
+
+        // Год издания книги
+        public int YearPublished { get; set; }
+
+        // Переопределение метода ToString для удобного вывода информации о книге
+        public override string ToString()
+        {
+            return $"ID: {Id}, Название: {Title}, Автор: {Author}, Год издания: {YearPublished}";
+        }
+    }
+}
---
+++ BookManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblio_book
+{
+    // Класс управляет коллекцией книг
+    public class BookManager
+    {
+        // Список книг в библиотеке
+        public List<Book> Books { get; private set; }
+
+        // Конструктор инициализирует пустой список книг
+        public BookManager()
+        {
+            Books = new List<Book>();
+        }
+
+        // Метод добавляет книгу в список
+        public void AddBook(Book book)
+        {
+            if (book == null) // Проверка, что книга не null
+                throw new ArgumentNullException(nameof(book), "Книга не может быть null.");
+
+            if (Books.Exists(b => b.Id == book.Id)) // Проверка, что ID книги уникален
+                throw new InvalidOperationException($"Книга с ID {book.Id} уже существует.");
+
+            Books.Add(book); // Добавление книги в список
+        }
+
+        // Метод удаляет книгу по её ID
+        public void RemoveBook(string bookId)
+        {
+            var book = GetBook(bookId); // Поиск книги по ID
+            if (book == null) // Проверка, что книга существует
+                throw new KeyNotFoundException($"Книга с ID {bookId} не найдена.");
+
+            Books.Remove(book); // Удаление книги из списка
+        }
+
+        // Метод возвращает книгу по её ID
+        public Book GetBook(string bookId)
+        {
+            return Books.Find(b => b.Id == bookId); // Поиск книги по ID
+        }
+
+        // Метод возвращает список всех книг
+        public List<Book> GetAllBooks()
+        {
+            return Books;
+        }
+    }
+}
